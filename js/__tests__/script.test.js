4e25bb468570bba5b35028255632fb8d5571185d
krzwier <krzwier@gmail.com>
2021-05-20 22:06:38 -0500

    Adds methods to fetch and display profile

--- js/__tests__/script.test.js
+++ js/__tests__/script.test.js
@@ -15,41 +15,74 @@ afterEach(() => {
     document.innerHTML = "";
 });
 
-describe('Gallery.fetchProfile', () => {
+describe('Gallery.fetchProfile()', () => {
+
+    const log = global.console.log; // save original console.log function
 
     beforeEach(() => {
         fetchMock.doMock();
+        fetch.resetMocks();
         console.log = jest.fn();
     });
 
     afterEach(() => {
-        fetch.resetMocks();
+        global.console.log = log; // restore original console.log after all tests
     })
 
     it('calls API and returns data', () => {
         const gallery = new Gallery();
-        fetch.mockResponseOnce({
-            "login": "octocat",
-            "id": 1,
-            "node_id": "MDQ6VXNlcjE=",
-            "avatar_url": "https://github.com/images/error/octocat_happy.gif",
-            "gravatar_id": "",
-            "url": "https://api.github.com/users/octocat",
-            "html_url": "https://github.com/octocat"
-        });
-        gallery.fetchProfile().then(res => {
+        fetch.mockResponseOnce(JSON.stringify({
+            login: "octocat",
+            id: 1,
+            node_id: "MDQ6VXNlcjE=",
+            avatar_url: "https://github.com/images/error/octocat_happy.gif",
+            gravatar_id: "",
+            url: "https://api.github.com/users/octocat",
+            html_url: "https://github.com/octocat"
+        }));
+            
+        gallery.fetchProfile().then((res) => {
             expect(res).toEqual({
-                "login": "octocat",
-                "id": 1,
-                "node_id": "MDQ6VXNlcjE=",
-                "avatar_url": "https://github.com/images/error/octocat_happy.gif",
-                "gravatar_id": "",
-                "url": "https://api.github.com/users/octocat",
-                "html_url": "https://github.com/octocat"
+                login: "octocat",
+                id: 1,
+                node_id: "MDQ6VXNlcjE=",
+                avatar_url: "https://github.com/images/error/octocat_happy.gif",
+                gravatar_id: "",
+                url: "https://api.github.com/users/octocat",
+                html_url: "https://github.com/octocat"
             });
+            expect(fetch.mock.calls.length).toEqual(1);
+            expect(fetch.mock.calls[0][0]).toEqual(`https://api.github.com/users/${gallery.username}`);
         });
-        expect(fetch.mock.calls.length).toEqual(1);
-        expect(fetch.mock.calls[0][0]).toEqual("https://api.github.com/users/krzwier");
+
+    });
+
+    it('logs to console when fetch fails', () => {
+        fetch.mockReject(new Error('Fetch failed'));
+        const gallery = new Gallery();
+        gallery.fetchProfile().then(() => {
+            expect(global.console.log.mock.calls[0][0]).toContain("Fetch failed in fetchProfile() method");
+        });
+
+    });
+
+});
+
+describe('Gallery.displayProfile()', () => {
+
+    it ('creates a new div inside overview element', () => {
+        const newDivs = document.querySelectorAll(".overview>div");
+        expect(newDivs.length).toBe(1);
+    });
+
+    it ('creates figure inside new div', () => {
+        const newFigs = document.querySelectorAll(".overview>div>figure");
+        expect(newFigs.length).toBe(1);
+    });
+
+    it ('creates info div inside new div', () => {
+        const infoDiv = document.querySelector(".overview>div>div");
+        expect(infoDiv.innerHTML).toContain("<p><strong>Name:</strong>");
     });
 
 });
--- js/main/script.js
+++ js/main/script.js
@@ -1,25 +1,51 @@
 class Gallery {
 
     // PUBLIC FIELDS
+    
+    username = "krzwier";
 
     // Overview div
     overview = document.querySelector(".overview");
 
-    username = "krzwier";
+    
 
 
     // PUBLIC METHODS
 
     async fetchProfile() {
-        const data = await fetch("https://api.github.com/users/krzwier");
-        const userData = await data.json();
-
+        try {
+            const data = await fetch(`https://api.github.com/users/${this.username}`);
+            const dataClone = data.clone();
+            const userData = await dataClone.json();
+            this.displayProfile(userData);
+            return userData;
+        } catch (e){
+             console.log(`Fetch failed in fetchProfile() method: ${e.message}`);
+        }
     }
 
+    displayProfile(userData) {
+        const newDiv = document.createElement("div");
+        newDiv.classList.add("user-info");
+        newDiv.innerHTML = `
+            <figure>
+                <img alt="user avatar" src="${userData.avatar_url}"} />
+            </figure>
+            <div>
+                <p><strong>Name:</strong> ${userData.name}</p>
+                <p><strong>Bio:</strong> ${userData.bio}</p>
+                <p><strong>Location:</strong> ${userData.location}</p>
+                <p><strong>Number of public repos:</strong> ${userData.public_repos}</p>
+            </div>
+        `;
+        this.overview.append(newDiv);
 
+    }
 }
 
 const gallery = new Gallery();
+
+gallery.fetchProfile();
 
 
 /* ---- EXPORT ONLY IF RUNNING TESTS ---- */
